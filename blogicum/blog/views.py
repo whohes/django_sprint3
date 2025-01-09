d0085f5e8fa14bac0041de9d9ffcc4e83cb080dc
Артур Денцель <dencelq_2007@mail.ru>
2025-01-09 17:33:21 +0600

    2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -10,7 +10,10 @@ def index(request):
         category__is_published=True
     ).order_by('-pub_date')[:5]
 
-    return render(request, 'blog/index.html', {'post_list': posts})
+    return render(request,
+                  'blog/index.html',
+                  {'post_list': posts}
+                  )
 
 
 def post_detail(request, id):
@@ -21,14 +24,23 @@ def post_detail(request, id):
         pub_date__lte=now(),
         category__is_published=True
     )
-    return render(request, 'blog/detail.html', {'post': post})
+    return render(request,
+                  'blog/detail.html',
+                  {'post': post}
+                  )
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+    category = get_object_or_404(Category,
+                                 slug=category_slug,
+                                 is_published=True
+                                 )
     posts = category.post_set.filter(
         is_published=True,
         pub_date__lte=now()
     ).order_by('-pub_date')
 
-    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+    return render(request,
+                  'blog/category.html',
+                  {'category': category, 'posts': posts}
+                  )
